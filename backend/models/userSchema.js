a7a7e73c45fcf116ab5dceed49be781863a5c2f7
Julia Janse <julia.broman@hotmail.com>
2026-04-15 09:23:45 +0200

    password hashed

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,6 +1,7 @@
 // imports of dependencies
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import next from "next";
 
 
 // schema for users
@@ -26,3 +27,18 @@ const userSchema = new mongoose.Schema({
         minLenght: 4
     }
 });
+
+// hashing av password
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.hash(this.password, salt);
+});
+
+// comparing password when user logging in
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const user = mongoose.model('user', userSchema);
+
+export default user;
