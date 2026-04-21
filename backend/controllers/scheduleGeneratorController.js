b050ad94651925021e4cfa5e7c911020a84aa0d1
Julia Janse <julia.broman@hotmail.com>
2026-04-21 15:41:47 +0200

    schedulegeneratorcontroller implemented

--- backend/controllers/scheduleGeneratorController.js
+++ backend/controllers/scheduleGeneratorController.js
@@ -1,8 +1,8 @@
 import Holidays from "date-holidays";
 import { eachDayOfInterval, isWeekend, format, startOfMonth, lastDayOfMonth, isSameDay } from 'date-fns';
-import userSchema from '../models/userSchema.js';
-import scheduleEntry from '../models/scheduleEntry.js';
-import monthSchedule from '../models/monthSchedule.js';
+import User from "../models/userSchema.js";
+import Schedule from "../models/scheduleEntry.js";
+import MonthSchedule from "../models/monthSchedule.js";
 
 // hantera svenska helgdagar
 const holiday = new Holidays('SE');
@@ -33,4 +33,129 @@ export function buildTaskPool(year, month) {
     }
 
     return taskPool;
+}
+
+// function som ska skapa ett schema för en månad utifrån de stallpass som ska tilldelas
+// hämtar användare och delar ut jämnt på alla hästägare
+export async function generateSchedule(taskPool, year, month) {
+    const usersFromDB = await User.find({ isActive: true, role: 'user' });
+
+
+    const userPool = usersFromDB.map(user => ({
+        id: user._id.toString(),
+        tasksLeft: user.requiredTasks,
+        lastTaskDate: null
+    }));
+
+    const scheduleEntries = [];
+
+    for (const task of taskPool) {
+        const assignedUserForTask = [];
+
+        for (let slot = 0; slot < task.slots; slot++) {
+            let horseOwners = userPool.filter(horseOwner => {
+                if (horseOwner.tasksLeft <= 0) {
+                    return false;
+                }
+
+                const alreadyBookedToday = scheduleEntries.some(entry => (
+                    entry.users.includes(horseOwner.id) && isSameDay(entry.date, task.date)
+                ));
+
+                if (alreadyBookedToday) {
+                    return false;
+                }
+
+                if (assignedUserForTask.includes(horseOwner.id)) {
+                    return false;
+                }
+
+                return true;
+            });
+
+            // sortering av hästägare 
+            // väljer de som har gjort minst pass först
+            horseOwners.sort((horseOwnerA, horseOwnerB) => {
+                const taskDifference = horseOwnerB.tasksLeft - horseOwnerA.tasksLeft;
+
+                if (taskDifference !== 0) {
+                    return taskDifference;
+                }
+
+                if (horseOwnerA.lastTaskDate === null && horseOwnerB.lastTaskDate !== null) {
+                    return -1;
+                }
+
+                if (horseOwnerA.lastTaskDate !== null && horseOwnerB.lastTaskDate === null) {
+                    return 1;
+                }
+
+                if (horseOwnerA.lastTaskDate === null && horseOwnerB.lastTaskDate === null) {
+                    return 0;
+                }
+
+                return horseOwnerA.lastTaskDate - horseOwnerB.lastTaskDate;
+            });
+
+            let selectedUser = horseOwners[0];
+
+            if (!selectedUser) {
+                throw new Error(`Kunde inte tilldela slot ${slot + 1} för passet ${task.taskType} den ${task.date.toDateString()}.`);
+            }
+
+            const userIndex = userPool.findIndex(owner => owner.id === selectedUser.id);
+
+            if (userIndex === -1) {
+                throw new Error('Internt fel: Hittade inte den valda ägaren.');
+            }
+
+            userPool[userIndex].tasksLeft -= 1;
+            userPool[userIndex].lastTaskDate = task.date;
+
+            assignedUserForTask.push(selectedUser.id);
+        }
+
+        scheduleEntries.push({
+            date: task.date,
+            taskType: task.taskType,
+            users: assignedUserForTask,
+        });
+    }
+
+    try {
+        const savedEntries = await Schedule.insertMany(scheduleEntries);
+        const savedEntryIds = savedEntries.map(entry => entry._id);
+
+        const newMonthSchedule = new MonthSchedule({
+            month: month,
+            year: year,
+            entries: savedEntryIds,
+        });
+        await newMonthSchedule.save();
+
+        return { success: true, message: `Schema genererat för ${year}-${month}.` };
+    } catch (error) {
+        console.error('Fel vid schemagenerering', error);
+        throw new Error('Schemat kunde inte slutföras eller sparas');
+    }
+}
+
+// hämtar schemat för en specifik månad
+export async function getScheduleByMonth(year, month) {
+    try {
+        const findSchedule = await MonthSchedule.findOne({ year, month })
+            .populate({
+                path: 'entries',
+                populate: { path: 'users', select: 'firstName lastName' }
+            });
+
+        if (!findSchedule) {
+            return null;
+        }
+
+        return findSchedule.entries;
+    } catch (error) {
+        console.error('Fel vid hämtning av schema:', error);
+        throw new Error('Kunde inte hämta schema');
+    }
 }
