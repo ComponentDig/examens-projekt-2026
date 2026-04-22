c221727dc99fca2edffb040a0b7c4f91bbf06bea
Julia Janse <julia.broman@hotmail.com>
2026-04-22 11:33:06 +0200

    added Outlet i AdminLayout

--- frontend/layouts/AdminLayout.jsx
+++ frontend/layouts/AdminLayout.jsx
@@ -1,20 +1,14 @@
+import { Outlet } from "react-router-dom";
 import AdminSidebar from "../admin/components/AdminSidebar";
 
-const AdminLayout = ({ children }) => {
+const AdminLayout = () => {
     return (
         <>
             <div className="flex min-h-screen">
                 <AdminSidebar />
-
-                <div className="flex-1 flex flex-col">
-                    <header className="h-16 bg-white flex items-center px-8">
-                        <h2 className="text-xl font-semibold text-admintext">Adminpanel - Stallportal</h2>
-                    </header>
-
-                    <main className="p-8">
-                        {children}
-                    </main>
-                </div>
+                <main className="flex-1 p-6">
+                    <Outlet />
+                </main>
             </div>
         </>
     )
