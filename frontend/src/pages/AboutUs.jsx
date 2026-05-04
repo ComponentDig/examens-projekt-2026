e4c9e7987623a67e33e9c65d294023bc5ce76703
Julia Janse <julia.broman@hotmail.com>
2026-05-04 13:57:38 +0200

    herosection on aboutus page done

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,15 +1,15 @@
-
+import HeroImage from '../assets/Tuna-vinter.jpg'
 function AboutUs() {
 
     return (
         <>
             {/* Hero-section? */}
-            <section className="">
-                <img src="" className="" alt="" />
-                <div></div>
-                <div className="">
-                    <h1 className="">Vi på Tuna</h1>
-                    <p className="">nån fin text</p>
+            <section className="relative h-[60vh] flex items-center justify-center">
+                <img src={HeroImage} className="absolute inset-0 w-full h-full object-cover" alt="" />
+                <div className='absolute inset-0 bg-black/40'></div>
+                <div className="relative text-center text-white px-4">
+                    <h1 className="text-4xl md:text-6xl font-secondary mb-4">Vi på Tuna</h1>
+                    <p className="text-lg md:text-xl max-w-2xl mx-auto font-light italic font-primary">nån fin text</p>
                 </div>
             </section>
 
