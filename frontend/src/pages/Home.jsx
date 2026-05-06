59c861cf4224f875500e232de97a9ccf0e38d00c
Julia Janse <julia.broman@hotmail.com>
2026-05-06 10:08:53 +0200

    added image to homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,26 @@
-
+import startImage from '../assets/Tuna-vinter2.jpg';
 
 
 function Home() {
 
     return (
         <>
-            <div className="font-primary max-w-3xl mx-auto text-center">
-                <div className="py-20 px-6">
-                    <h1 className="text-3xl md:text-4xl font-semibold mb-6">Välkommen till stall Tuna</h1>
-                    <div className="text-lg mb-4">
-                        <p className="text-lg mb-4">Här möts intresset för islandshästar i en lugn och naturnära miljö - där samarbete och glädjen för hästlivet står i centrum.</p>
-                        {/* en länk som går till att boka banan */}
-                        <p>På gården finns en fin ovalbana som kan bokas för träning och tävling. Vårat stall en perfekt plats för att rida, umgås och njuta av tiden i stallet.</p>
+            <div className='py-16 px-16 max-w-6xl mx-auto font-primary'>
+                <div className='flex flex-col md:flex-row items-center gap-12'>
+                    <div className='w-full md:w-1/2'>
+                        <div className='rounded-2xl overflow-hidden shadow-2xl'>
+                            <img src={startImage} className='w-full h-full object-cover' />
+                        </div>
                     </div>
-                    <p className="text-lg">På stall Tuna vill vi att det ska vara en plats där man trivs och känner sig välkommen - här är vi som en stor familj.</p>
+
+                    <div className='w-full md:w-1/2'>
+                        <h1 className='text-5xl font-bold mb-6'>Välkommen till Stall Tuna</h1>
+                        <p className='text-lg mb-4'>Här möts intresset för islandshästar i en lugn och naturnära miljö - där samarbete och glädjen för hästlivet står i centrum.</p>
+                        <p className='mb-4'>På gården finns en fin ovalbana som kan bokas för träning och tävling. Vårat stall en perfekt plats för att rida, umgås och njuta av tiden i stallet.</p>
+                        <p>På stall Tuna vill vi att det ska vara en plats där man trivs och känner sig välkommen - här är vi som en stor familj.</p>
+                    </div>
                 </div>
             </div>
-
-
-
         </>
     )
 
