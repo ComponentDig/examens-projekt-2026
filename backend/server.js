e50de6bdaa078a98fbe36ee318c5e5c596c0fe58
Julia Janse <julia.broman@hotmail.com>
2026-05-13 10:26:20 +0200

    reversed changes

--- backend/server.js
+++ backend/server.js
@@ -28,24 +28,5 @@ mongoose
         server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
     })
     .catch((error) => console.log(error));
-let isConnected = false;
-
-const connectDB = async () => {
-    if (isConnected) return;
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        isConnected = true;
-        console.log("MongoDB Connected");
-    } catch (err) {
-        console.log("MongoDB Connection Error:", err);
-    }
-};
-
-// På Vercel körs inte .listen() på samma sätt. 
-// Vi ansluter till databasen vid varje anrop (serverless style)
-server.use(async (req, res, next) => {
-    await connectDB();
-    next();
-});
 
 export default server;
