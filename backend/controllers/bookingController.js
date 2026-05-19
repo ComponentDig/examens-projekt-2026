8f94db01bbe0cf3fde0091f010a86140f2260df9
Julia Janse <julia.broman@hotmail.com>
2026-05-19 18:26:12 +0200

    createbooking and getbookings implemented in bookingController

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -4,16 +4,38 @@ export const createBooking = async (req, res) => {
 
     try {
 
-        const { date, type, startTime, endTime } = req.body;
+        const { guestName, guestPhone, guestEmail, date, type, startTime, endTime } = req.body;
 
         const newBooking = await Booking.create({
+
+            user: req.user ? req.user._id : null,
             date,
             type,
             startTime,
-            endTime
+            endTime,
+            guestName,
+            guestPhone,
+            guestEmail
         });
 
-    } catch (error) {
+        res.status(201).json(newBooking);
 
+    } catch (error) {
+        console.error("Fel vid bokning", error);
+        res.status(500).json({ message: "Kunde inte skapa bokningen" });
+    }
+}
+
+export const getBookings = async (req, res) => {
+
+    try {
+        const bookings = await Booking.find();
+
+        res.status(200).json(bookings);
+
+
+    } catch (error) {
+        console.error("Kunde inte hämta bokningar");
+        res.status(500).json({ message: "Kunde inte hämta" });
     }
 }
