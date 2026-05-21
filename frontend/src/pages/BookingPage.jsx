ee6b6aa793a4552a3550c3ce3161b1b49c64192f
Julia Janse <julia.broman@hotmail.com>
2026-05-21 10:53:12 +0200

    bookings working

--- frontend/src/pages/BookingPage.jsx
+++ frontend/src/pages/BookingPage.jsx
@@ -37,6 +37,41 @@ function BookingPage() {
 
     }, [])
 
+    const handleBookingSubmit = async (e) => {
+        e.preventDefault();
+
+        const bookingData = {
+            date: selectedDate.toLocaleDateString('sv-SE'),
+            bookingType: bookingType,
+            startTime: selectedHour,
+            guestName: name,
+            guestEmail: email,
+            guestPhone: phone
+        };
+
+        try {
+
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/bookings`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(bookingData)
+            });
+
+            if (response.ok) {
+                alert("Bokningen har registrerats!");
+                setIsModalOpen(false);
+
+            } else {
+                alert("Något gick fel vid bokningen. Försök igen.");
+            }
+
+        } catch (error) {
+            console.error("Kunde inte skicka bokning:", error);
+            alert("Kunde inte ansluta till servern.");
+        }
+    };
+
+
     const formattedSelectedDate = selectedDate.toLocaleDateString('sv-SE');
 
     const takenHours = bookings.filter(booking => {
@@ -151,7 +186,7 @@ function BookingPage() {
                             <h3 className="text-2xl font-bold mb-4">Slutför bokning</h3>
                             <p className="text-sm text-gray-500 mb-6">Du håller på att boka den {selectedDate.toLocaleDateString('sv-SE')}.</p>
 
-                            <form onSubmit={(e) => { e.preventDefault(); alert("Bokad!"); }} className="space-y-4">
+                            <form onSubmit={handleBookingSubmit} className="space-y-4">
                                 <div>
                                     <label className="block text-sm font-semibold text-gray-700 mb-1">Namn</label>
                                     <input type="text" placeholder="för- och efternamn" value={name} onChange={(e) => setName(e.target.value)} required className="w-full border border-gray-300 rounded-xl px-4 py-2 text-sm focus:outline-none focus:border-secondarycolor" />
